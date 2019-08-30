2e6205ee706b7621a76ff6646b2e0e2ecc2837d8
nangongyibin7219 <nangongyibin@gmail.com>
2019-08-30 17:33:54 +0800

    sql注入

---
+++ src/main/java/com/ngyb/study/jdbc/Login.java
@@ -0,0 +1,35 @@
+package com.ngyb.study.jdbc;
+
+import java.sql.*;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2019/8/30 17:11
+ */
+public class Login {
+    public static void main(String[] args) {
+        loginIn("ngyb", "Liang14c");
+    }
+
+    public static void loginIn(String username, String password) {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://it.nangongyibin.com:3306/ngyb", "root", "Liang14c");
+            String sql = "select * from login where name = ? and pwd = ?";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                System.out.println("登录成功");
+            } else {
+                System.out.println("登录失败");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("登录失败");
+        }
+
+    }
+}
